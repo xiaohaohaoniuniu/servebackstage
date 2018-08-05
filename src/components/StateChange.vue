f3bf4f4caa03917ca1551bfec7537a77848b4bed
daniu <2808613957@qq.com>
2018-08-05 23:21:09 +0800

    可以将进行中改变成待评价

--- src/components/StateChange.vue
+++ src/components/StateChange.vue
@@ -1,32 +1,36 @@
 <template>
 	<div class="stateChange">
-		<!-- <ul>
-			<li v-for="state in stateList">
-			{{state.addr}}</li>
-		</ul> -->
+		<div class="loading" v-show="loading">
+			<img src="../assets/loading.gif" alt="">正在加载，请稍后...
+		</div>
 		<table class="table table-condensed table-hover">
 			<thead>
 				<tr>
-					<th>名字</th>
+					<th>服务号ID</th>
+					<th>服务内容</th>
+					<th>状态</th>
 					<th>地址</th>
-					<th>功能</th>
-					<th>距离</th>
-					<th>评论个数</th>
-					<th>单数</th>
-					<th>总评分</th>
+					<th>时间</th>
+					<th>价格</th>
+					<th>留言</th>
 					<th>操作</th>
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
-					<td>7</td>
-					<td>6</td>
-					<td>5</td>
-					<td>4</td>
-					<td>2</td>
-					<td>2</td>
-					<td>2</td>
-					<td>1</td>
+				<tr v-for="state in stateList">
+					<td>{{state.serviceNmber}}</td>
+					<td>{{state.project}}</td>
+					<td>{{state.state}}</td>
+					<td>{{state.addr}}</td>
+					<td>{{state.time}}</td>
+					<td>{{state.price}}</td>
+					<td>{{state.leaveWord}}</td>
+					<td v-if="state.state == '进行中'" v-on:click="stateChange(state.serviceNmber)">
+						<button class="btn btn-sm btn-info">{{state.state}}</button>
+					</td>
+					<td v-else>
+						<button class="btn btn-sm btn-info" disabled>{{state.state}}</button>
+					</td>
 				</tr>
 			</tbody>	
 		</table>
@@ -38,16 +42,36 @@
 		name:'stateChange',
 		data(){
 			return {
-				stateList:[]
+				stateList:[],
+				loading:true
+			}
+		},
+		methods:{
+			stateChange(serviceNmber){
+				var changeStateWc = this.stateList.filter(function(item){
+					return item.serviceNmber == serviceNmber
+				})
+				alert('已变成待评价状态');
+				changeStateWc[0].state = "待评价";
+				// 删除这条
+				axios.delete('/appointList/' + changeStateWc[0].appointId + ".json")
+				.then(res=>{
+					console.log('删除成功')
+				})
+				// 然后在写入
+				axios.post('/appointList.json',changeStateWc[0])
+				.then(res=>{
+					console.log("写入成功");
+				})
 			}
 		},
 		created(){
 			axios.get('/appointList.json')
 			.then(res=>{
-				// console.log(res.data);
+				this.loading = false;
 				var stateList = [];
 				for(var k in res.data){
-					res.data.stateId = k;
+					res.data[k].appointId = k;
 					stateList.push(res.data[k])
 				}
 				this.stateList = stateList;
@@ -56,6 +80,12 @@
 		}
 	}
 </script>
-<style>
-	
+<style scoped>
+	.loading{
+		text-align: center;
+	}
+	.loading img{
+		width: 20px;
+		margin: 20px;
+	}
 </style>
